27a4b8ab8cdb274ef03b51f0d9aef5f1965afbbc
Angelozimariami <m.s.gorgadzemari@gmail.com>
2022-03-31 07:45:13 -0700

    first commit

---
+++ smt.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n";
+  cout << "My Name is Mary \n";  
+  int myNum = 15;
+  cout << "I am " << myNum << " Years Old";
+  return 0;
+}
